af48a98c0506f09826cd6ceef84314c4070dba5b
࿇•ẞᗴᗩSԵ•࿇ <80647839+beastzx18@users.noreply.github.com>
2021-07-29 20:26:19 +0530

    Update stringsetup.py

--- stringsetup.py
+++ stringsetup.py
@@ -19,3 +19,5 @@ API_HASH = input("Enter API HASH here: ")
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
     print(client.session.save())
     client.send_message("me", client.session.save())
+
+# ....
